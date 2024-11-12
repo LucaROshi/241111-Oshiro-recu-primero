6c47321868f85dfffd0b4182b237d7f0aea639c6
LucaROshi <lucaoshiro@gmail.com>
2024-11-11 21:14:25 -0300

    recuperatorio

--- examen/src/main/java/com/example/demo/ExamenApplication.java
+++ examen/src/main/java/com/example/demo/ExamenApplication.java
@@ -1,28 +1,38 @@
 package com.example.demo;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
-@RequestMapping("/promedio")
+@Controller
+@RequestMapping("/teorico")
 @SpringBootApplication
+
 public class ExamenApplication {
 	public ArrayList<Integer> generador() {
 		ArrayList<Integer> numeros = new ArrayList<Integer>();
-		numeros.add(19);
-		numeros.add(34);
-		numeros.add(4);
-		numeros.add(45);
-		numeros.add(8);
+		Random random = new Random();
+		for(int i = 1; i < 5; i++) {
+			numeros.add(random.nextInt(100));
+		}
 		return numeros;
 	}
-	public void promedio() {
-		if() {
-			
-		}
+	@GetMapping("/promedio")
+	public String promedio() {
+		int suma = 0;
+		int promedio = 0;
+		suma = suma + numeros;
+		promedio = promedio + suma / numeros;
+		return ("promedio de numeros: " + promedio);
+	}
+	@GetMapping("/teorico")
+	public String teorico() {
+		return "La sobreescritura es un proceso que consiste en redefinir un método de una clase padre en una clase hija que hereda de ella. La sobrecarga de métodos es una característica de programación que consiste en definir varios métodos con el mismo nombre, pero con diferentes listas de parámetros.";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ExamenApplication.class, args);
